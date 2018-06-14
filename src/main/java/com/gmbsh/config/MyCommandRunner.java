f6c3a1aac9c3eb12098829e8448982818b94f241
shil <s52031564>
2018-06-14 09:56:18 +0800

    浏览器自动打开(需要配置状态)

---
+++ src/main/java/com/gmbsh/config/MyCommandRunner.java
@@ -0,0 +1,40 @@
+package com.gmbsh.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.awt.*;
+import java.net.URI;
+
+/**
+ * 项目启动浏览器自动打开
+ */
+@Component
+public class MyCommandRunner implements CommandLineRunner {
+    private static Logger logger = LoggerFactory.getLogger(MyCommandRunner.class);
+    @Value("${spring.web.loginurl}")
+    private String loginUrl;
+
+    @Value("${spring.web.googleexcute}")
+    private String googleExcutePath;
+
+    @Value("${spring.auto.openurl}")
+    private boolean isOpen;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(isOpen){
+            String cmd = googleExcutePath +" "+ loginUrl;
+            Runtime run = Runtime.getRuntime();
+            try{
+                run.exec(cmd);
+                logger.debug("启动浏览器打开项目成功");
+            }catch (Exception e){
+                e.printStackTrace();
+                logger.error(e.getMessage());
+            }
+        }
+    }
+}
