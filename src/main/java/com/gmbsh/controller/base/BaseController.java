9f979d62ce2c2f3a160b23227fdc4e5f02e4fe38
shil <s52031564>
2018-06-14 11:13:46 +0800

    basecontroller

---
+++ src/main/java/com/gmbsh/controller/base/BaseController.java
@@ -0,0 +1,54 @@
+package com.gmbsh.controller.base;
+
+
+import javax.servlet.http.HttpServletRequest;
+import com.gmbsh.util.Logger;
+import com.gmbsh.util.PageData;
+import com.gmbsh.util.UuidUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@Controller
+public class BaseController {
+
+	protected Logger logger = Logger.getLogger(this.getClass());
+
+	 /**
+	 * 得到PageData
+	 */
+	public PageData getPageData(){
+		return new PageData(this.getRequest());
+	}
+	
+	/**
+	 * 得到ModelAndView
+	 */
+	public ModelAndView getModelAndView(){
+		return new ModelAndView();
+	}
+	
+	/**
+	 * 得到request对象
+	 */
+	public HttpServletRequest getRequest() {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		
+		return request;
+	}
+
+	/**
+	 * 得到32位的uuid
+	 * @return
+	 */
+	public String get32UUID(){
+		
+		return UuidUtil.get32UUID();
+	}
+
+	
+}
